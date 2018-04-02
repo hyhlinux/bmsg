c2bed79848f469d3fa670d5ae61e31d34bf62ee8
huoyinghui <huoyinghui@apkpure.com>
2018-04-02 18:32:39 +0800

    add: CURD router

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -7,6 +7,46 @@ import (
 
 func init() {
 
+	beego.GlobalControllerRouter["bmsg/controllers:FromUserIdController"] = append(beego.GlobalControllerRouter["bmsg/controllers:FromUserIdController"],
+		beego.ControllerComments{
+			Method: "Post",
+			Router: `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["bmsg/controllers:FromUserIdController"] = append(beego.GlobalControllerRouter["bmsg/controllers:FromUserIdController"],
+		beego.ControllerComments{
+			Method: "GetAll",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["bmsg/controllers:FromUserIdController"] = append(beego.GlobalControllerRouter["bmsg/controllers:FromUserIdController"],
+		beego.ControllerComments{
+			Method: "Put",
+			Router: `/:Id`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["bmsg/controllers:FromUserIdController"] = append(beego.GlobalControllerRouter["bmsg/controllers:FromUserIdController"],
+		beego.ControllerComments{
+			Method: "Delete",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["bmsg/controllers:FromUserIdController"] = append(beego.GlobalControllerRouter["bmsg/controllers:FromUserIdController"],
+		beego.ControllerComments{
+			Method: "Get",
+			Router: `/:objectId`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
 	beego.GlobalControllerRouter["bmsg/controllers:MessgeController"] = append(beego.GlobalControllerRouter["bmsg/controllers:MessgeController"],
 		beego.ControllerComments{
 			Method: "Post",
@@ -40,6 +80,54 @@ func init() {
 			Params: nil})
 
 	beego.GlobalControllerRouter["bmsg/controllers:MessgeController"] = append(beego.GlobalControllerRouter["bmsg/controllers:MessgeController"],
+		beego.ControllerComments{
+			Method: "UpdateMessge",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["bmsg/controllers:MessgeController"] = append(beego.GlobalControllerRouter["bmsg/controllers:MessgeController"],
+		beego.ControllerComments{
+			Method: "Get",
+			Router: `/:objectId`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["bmsg/controllers:ToUserIdController"] = append(beego.GlobalControllerRouter["bmsg/controllers:ToUserIdController"],
+		beego.ControllerComments{
+			Method: "Post",
+			Router: `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["bmsg/controllers:ToUserIdController"] = append(beego.GlobalControllerRouter["bmsg/controllers:ToUserIdController"],
+		beego.ControllerComments{
+			Method: "GetAll",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["bmsg/controllers:ToUserIdController"] = append(beego.GlobalControllerRouter["bmsg/controllers:ToUserIdController"],
+		beego.ControllerComments{
+			Method: "Put",
+			Router: `/:Id`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["bmsg/controllers:ToUserIdController"] = append(beego.GlobalControllerRouter["bmsg/controllers:ToUserIdController"],
+		beego.ControllerComments{
+			Method: "Delete",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["bmsg/controllers:ToUserIdController"] = append(beego.GlobalControllerRouter["bmsg/controllers:ToUserIdController"],
 		beego.ControllerComments{
 			Method: "Get",
 			Router: `/:objectId`,
--- routers/router.go
+++ routers/router.go
@@ -15,9 +15,30 @@ import (
 
 func init() {
 	ns := beego.NewNamespace("/v1/api",
+		//beego.NSNamespace("/msg",
+		//	beego.NSInclude(
+		//		&controllers.MessgeController{},
+		//	),
+		//),
 		beego.NSNamespace("/msg",
-			beego.NSInclude(
-				&controllers.MessgeController{},
+			//CRUD Create(创建)、Read(读取)、Update(更新)和Delete(删除)
+			beego.NSNamespace("/create",
+				// by FromUserId 发送一封邮件
+				beego.NSRouter("/from", &controllers.FromUserIdController{}, "post:CreateMessge"),
+			),
+			beego.NSNamespace("/read",
+				//by ToUserId 查看自己收到的邮件.
+				beego.NSRouter("/to", &controllers.ToUserIdController{}, "get:ShowToUserMessges"),
+				//by FromUserId
+				beego.NSRouter("/from", &controllers.FromUserIdController{}, "get:ShowFromUserMessges"),
+			),
+			beego.NSNamespace("/update",
+				//by messge id -> m.status
+				beego.NSRouter("/", &controllers.MessgeController{}, "post:UpdateMessge"),
+			),
+			beego.NSNamespace("/delete",
+				//by messge id 删除已读信息
+				beego.NSRouter("/", &controllers.MessgeController{}, "delete:DeleteMessge"),
 			),
 		),
 	)
